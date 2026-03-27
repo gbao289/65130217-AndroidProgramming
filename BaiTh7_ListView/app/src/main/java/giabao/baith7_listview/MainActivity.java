b697e06736be99bb5dd43bffd2536ffa049dfd78
Võ Lê Gia Bảo <baovo.280905@gmail.com>
2026-03-27 16:47:54 +0700

    Add event OnitemClick

--- BaiTh7_ListView/app/src/main/java/giabao/baith7_listview/MainActivity.java
+++ BaiTh7_ListView/app/src/main/java/giabao/baith7_listview/MainActivity.java
@@ -1,8 +1,11 @@
 package giabao.baith7_listview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         //B1 Khai bao bien, them du lieu
         ArrayList<String> dsTenTinhThanhVN; // Khai Bao
         dsTenTinhThanhVN = new ArrayList<String>();
-         //Them Du Lieu
+        //Them Du Lieu
         dsTenTinhThanhVN.add("Ha Noi");
         dsTenTinhThanhVN.add("Thanh pho Ho Chi Minh");
         dsTenTinhThanhVN.add("Nha Trang");
@@ -33,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         dsTenTinhThanhVN.add("Da Nang");
         //B2 tao adapter
         ArrayAdapter<String> adapterTinhThanh;
-        adapterTinhThanh = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,dsTenTinhThanhVN);
+        adapterTinhThanh = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dsTenTinhThanhVN);
         //B3 Gan vao dieu khien hien thi listView
         //3.1 Tim dieu khien
         ListView lvTenTinhThanh = findViewById(R.id.lvDanhSachTT);
@@ -42,5 +45,16 @@ public class MainActivity extends AppCompatActivity {
         //3.3 Lang nghe va xu ly su kien
 
 
+        AdapterView.OnItemClickListener BoLangNghevaXl = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Code xu ly o day
+                // position la vi tri phan tu duoc click
+                // vi du hien thi 1 tin nhan len mang hinh
+                Toast.makeText(MainActivity.this, "Ban vua chon " + dsTenTinhThanhVN.get(position) , Toast.LENGTH_LONG).show();
+            }
+        };
+        lvTenTinhThanh.setOnItemClickListener(BoLangNghevaXl);
     }
-}
+
+    }
