07b945b136f0f564ed53b641e0f945c234a4dbe2
Võ Lê Gia Bảo <baovo.280905@gmail.com>
2026-04-20 07:49:00 +0700

    Lần thứ 2

--- ThiGK/app/src/main/java/thigk2/volegiabao/thigk/MainActivity.java
+++ ThiGK/app/src/main/java/thigk2/volegiabao/thigk/MainActivity.java
@@ -1,20 +1,55 @@
 package thigk2.volegiabao.thigk;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+
+
+        if (savedInstanceState == null) {
+            loadFragment(new ChucNang1Fragment());
+        }
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_tinhtoan) {
+                selectedFragment = new ChucNang1Fragment();
+            } else if (itemId == R.id.nav_search) {
+                selectedFragment = new ChucNang2Fragment();
+            } else if (itemId == R.id.nav_dulich) {
+                selectedFragment = new ChucNang3Fragment();
+            } else if (itemId == R.id.nav_profile) {
+                selectedFragment = new ChucNang4Fragment();
+                return true;
+            }
+
+            if (selectedFragment != null) {
+                loadFragment(selectedFragment);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
     }
 }
